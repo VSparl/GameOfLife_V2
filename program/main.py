14512aed700f5ff39aa66d1df259cbca0dcf3d15
ZuffXD <zuffxd@gmail.com>
2023-12-13 15:12:01 +0100

    update board function, not done

--- program/main.py
+++ program/main.py
@@ -1,12 +1,19 @@
 """Making a module docstring so my linter shuts up"""
 import msvcrt
 import random
+from time import sleep
 from colorama import Back
 
 
 def display_welcome() -> None:
     """Print some welcome words."""
     print("Welcome to the Game of Life, blah blah blah...")
+
+
+def end_game() -> None:
+    """Finish the game and display some text as the end"""
+    print("Game finished.")
+    quit()
 
 
 def controlled_input(input_string: str, max_len: int) -> list:
@@ -85,19 +92,72 @@ def generate_random_board(height: int, width: int) -> list[list[bool]]:
     return local_board
 
 
+def count_neighbors(board, row, col):
+    """Count the number of live neighbors for a given cell."""
+    neighbors = [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+                 (row, col - 1),                     (row, col + 1),
+                 (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
+
+    live_neighbors = 0
+    for i, j in neighbors:
+        if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j]:
+            live_neighbors += 1
+
+    return live_neighbors
+
+def update_board(board):
+    """Update the board according to the rules of the Game of Life."""
+    # Initialize a board where all cells are dead
+    new_board: list[list[bool]] = [[False] * len(board[0]) for _ in range(len(board))]
+
+    for i, _ in enumerate(board):
+        for j, counter in enumerate(board[i]):
+            live_neighbors = count_neighbors(board, i, j)
+
+            # Apply Game of Life rules
+            #     live cell has 2 or 3 neighbours           cell is a birth cell -> 3 neighbours
+            if (counter and (live_neighbors in (2, 3))) or (not counter and live_neighbors == 3):
+                new_board[i][j] = True
+
+    return new_board
+
+
 def print_board(local_board):
     """Print the current state of the board.
 
     Colorama is used to draw colored characters.
     Live cells are displayed as green."""
+    live_cells = 0
+
     for row in local_board:
         for cell in row:
             # Color only if cell is alive
-            print(f"{Back.GREEN}  {Back.RESET}" if cell else "  ", end='')
-        print()  # New line
+            if cell:
+                print(f"{Back.GREEN}▯ {Back.RESET}", end='')
+                live_cells += 1
+            else:
+                print("▯ ", end='')
+        print()  # New line after row
+
+    if live_cells == 0:
+        end_game()
 
 
 if __name__ == "__main__":
     display_welcome()
-    global_board = generate_random_board(20, 30)
+    global_board = generate_random_board(5, 5)
+    last_board = []
+
+    while last_board != global_board:
+        print_board(global_board)
+        print("\n" + "-" * 20 + "\n")
+        last_board = global_board
+        global_board = update_board(global_board)
+        sleep(0.2)
+        input()
+
     print_board(global_board)
+    print("\n" + "-" * 20 + "\n")
+    last_board = global_board
+    global_board = update_board(global_board)
+    sleep(0.2)
