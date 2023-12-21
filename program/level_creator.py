88c7fa895e0ef0496b25e07b4225a6fccc3cca94
ZuffXD <zuffxd@gmail.com>
2023-12-21 11:07:55 +0100

    Added level creator

---
+++ program/level_creator.py
@@ -0,0 +1,61 @@
+"""Docstring so my linter shuts up"""
+import msvcrt
+import sys
+
+def controlled_input(input_string: str, max_len: int) -> list:
+    """Input function but with max length
+
+    Automatically returns and goes on to the next line when the specified
+    input length has been reached.
+    Still allows for Ctrl+C to interrupt the program.
+    This function was written with the help of ChatGPT.
+
+    WARNING: This only works on Windows devices.
+    """
+    input_chars: list[str] = []
+    print(input_string, end='', flush=True)
+    while True:
+        if msvcrt.kbhit():  # React to keyboard input
+            # Get typed character from keyboard and decode it
+            char = msvcrt.getch().decode('utf-8')
+            # Handle all exceptions and special keys
+            if char == '\x03':  # Ctrl+C
+                # Simulate same behaviour of "regular" Ctrl+C
+                raise KeyboardInterrupt
+
+            if char == '\x08':  # Backspace key
+                if input_chars:
+                    input_chars.pop()
+                    # Move cursor back, overwrite character with a space
+                    print('\b \b', end='', flush=True)
+                continue
+
+            if char == '\r':  # Enter key
+                # Function returns automatically
+                continue
+
+            # Add typed character to list and print it to the screen
+            input_chars.append(char)
+            print(char, end='', flush=True)
+
+            if len(input_chars) >= max_len:
+                # Desired length reached
+                break
+
+    print()
+    return input_chars
+
+FILENAME: str = input("Enter filename (without extension): ")
+WIDTH: int = int(input("How wide? (chars) "))
+HEIGHT: int = int(input("How high? (chars) "))
+
+with open(f"{FILENAME}.gol", "w", encoding="utf-8") as fp:
+    for i in range(HEIGHT):
+        fp.write("".join(controlled_input(f"Enter line No. {i + 1}: ", WIDTH)))
+
+        if i != HEIGHT - 1:
+            # Don't write last newline
+            fp.write("\n")
+
+print(f"Successfully created file under {FILENAME}.gol")
+sys.exit(0)
