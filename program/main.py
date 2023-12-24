2e09a1a4bd460f32395da8d7542c4c59151118f1
ZuffXD <zuffxd@gmail.com>
2023-12-24 23:55:22 +0200

    tyding up code & output, refined file imports, auto-restart (incomplete)

--- program/main.py
+++ program/main.py
@@ -4,13 +4,60 @@ import random
 import os
 import sys
 from time import sleep
-from colorama import Back
+from colorama import Back, Fore
+
+
+class FileInvalidError(Exception):
+    """Custom error for .gol files that don't pass the validity check."""
+    def __init__(
+        self,
+        message="File didn't pass the validity check. "
+        "All characters must be printable and all lines must have the same length."):
+        self.message = message
+        super().__init__(self.message)
 
 
 def display_welcome() -> None:
     """Print some welcome words."""
     os.system("cls")
     print("Welcome to the Game of Life, blah blah blah...")
+
+
+def get_start_board() -> list[list[bool]]:
+    """Handle and return a board based on the command line arguments."""
+    # Get filename to import from command line args
+    filename: str = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    if filename:
+        try:
+            local_board = import_from_file(sys.argv[1])
+
+        except FileNotFoundError:
+            # Start level editor
+            print(f"{Fore.RED}ERROR:{Fore.RESET} Your file was not found.",
+                "The level editor will start so you can make",
+                "your own board. \n")
+
+            # Prompt user to create their own board with the filename
+            create_level(filename)
+
+            print("\nThe program needs to restart to apply your changes")
+            for n in range(3):
+                print(f"{Fore.YELLOW}WARNING: {Fore.RESET}Restarting in {3 - n}...", end="\r")
+                sleep(1)
+
+                # Restart program with same args
+                # TODO Program remains stuck here after completing, requres Enter to "wake up" again
+                os.execv(sys.executable, [sys.executable] + sys.argv)
+
+        except FileInvalidError:
+            print(f"{Fore.RED}ERROR: {Fore.RESET}File invalid. Try again with another file.")
+            sys.exit(1)
+
+    else:
+        local_board = generate_random_board()
+
+    return local_board
 
 
 def get_starting_position(height: int, width: int) -> list[list[bool]]:
@@ -22,9 +69,11 @@ def get_starting_position(height: int, width: int) -> list[list[bool]]:
     local_board: list[list[bool]] = []
 
     for i in range(height):
-        usr_input: list[str] = controlled_input(f"Enter row No. {i + 1}: ", width)
+        usr_input: list[str] = controlled_input(
+            f"Enter row No. {i + 1}: ", width
+        )
         # Turn non-space characters into True vaules and spaces into False
-        processed_input = [not char == ' ' for char in usr_input]
+        processed_input = [not char == " " for char in usr_input]
         local_board.append(processed_input)
 
     return local_board
@@ -41,30 +90,30 @@ def controlled_input(input_string: str, max_len: int) -> list:
     WARNING: This only works on Windows devices.
     """
     input_chars: list[str] = []
-    print(input_string, end='', flush=True)
+    print(input_string, end="", flush=True)
     while True:
         if msvcrt.kbhit():  # React to keyboard input
             # Get typed character from keyboard and decode it
-            char = msvcrt.getch().decode('utf-8')
+            char = msvcrt.getch().decode("utf-8")
             # Handle all exceptions and special keys
-            if char == '\x03':  # Ctrl+C
+            if char == "\x03":  # Ctrl+C
                 # Simulate same behaviour of "regular" Ctrl+C
                 raise KeyboardInterrupt
 
-            if char == '\x08':  # Backspace key
+            if char == "\x08":  # Backspace key
                 if input_chars:
                     input_chars.pop()
                     # Move cursor back, overwrite character with a space
-                    print('\b \b', end='', flush=True)
+                    print("\b \b", end="", flush=True)
                 continue
 
-            if char == '\r':  # Enter key
+            if char == "\r":  # Enter key
                 # Function returns automatically
                 continue
 
             # Add typed character to list and print it to the screen
             input_chars.append(char)
-            print(char, end='', flush=True)
+            print(char, end="", flush=True)
 
             if len(input_chars) >= max_len:
                 # Desired length reached
@@ -74,13 +123,13 @@ def controlled_input(input_string: str, max_len: int) -> list:
     return input_chars
 
 
-def generate_random_board(height: int=-1, width: int=-1) -> list[list[bool]]:
+def generate_random_board(height: int = -1, width: int = -1) -> list[list[bool]]:
     """Generates a random starting configuration of a board.
 
     Arguments specify the size of the board.
-    A cell has a 25% chance to contain a counter.
+    A cell has a 50% chance to contain a counter.
 
-    If the args are -1 for both height and width, the numbers will be 
+    If the args are -1 for both height and width, the numbers will be
     selected so that the game consumes the entire screen.
     """
     local_board: list[list[bool]] = []
@@ -93,9 +142,8 @@ def generate_random_board(height: int=-1, width: int=-1) -> list[list[bool]]:
         # One cell is 2 chars wide
         width = terminal.columns // 2
 
-
     for _ in range(height):
-        processed_input = [(random.random() <= 0.25) for _ in range(width)]
+        processed_input = [(random.random() <= 0.5) for _ in range(width)]
         local_board.append(processed_input)
 
     return local_board
@@ -103,24 +151,30 @@ def generate_random_board(height: int=-1, width: int=-1) -> list[list[bool]]:
 
 def import_from_file(filepath: str) -> list[list[bool]]:
     """Turns a file into a proper list and checks the validity of the board."""
-    local_board = []
-    line = True
+    local_board: list[list[bool]] = []
+    line: str = "True"
 
-    with open(filepath, "r", encoding="utf-8") as fp:
-        print("file found")
+    # Add file extension if it wasn't provided
+    if filepath[-4:] != ".gol":
+        filepath += ".gol"
+
+    with open(os.path.join("..", "boards", filepath), "r", encoding="utf-8") as fp:
+        print(f"{Fore.GREEN}SUCCESS: {Fore.RESET}File found, processing...")
+        sleep(1.2)
+
         while line:
             line = fp.readline().strip("\n")
             # Convert char to bool value
-            processed_line = [not char == ' ' for char in line]
+            processed_line = [not char == " " for char in line]
             if processed_line:
-                # Is not empty
+                # Avoids empty lists, they exist for some reason
                 local_board.append(processed_line)
 
     if check_validity(local_board):
         return local_board
 
-    print("validity check not passed")
-    return False
+    # File is faulty
+    raise FileInvalidError
 
 
 def check_validity(board: list[list[bool]]) -> bool:
@@ -129,7 +183,7 @@ def check_validity(board: list[list[bool]]) -> bool:
     Count the different lengths of rows on a board.
     All rows should be the same length.
     """
-    last_col = 0
+    last_col = -1
     diff_cols = 0
     for _, col in enumerate(board[:-1]):
         curr_col = len(col)
@@ -144,20 +198,37 @@ def check_validity(board: list[list[bool]]) -> bool:
 
 def create_level(name: str) -> None:
     """Create a level according to user specifications.
-    
+
     The new level will be saved in a .gol file as characters.
     """
-    width: int = int(input("How wide? (chars) "))
-    height: int = int(input("How high? (chars) "))
+    while True:
+        width: int = int(input("How wide? (chars) "))
+        if width <= 1:
+            print("Please enter a value of at least two characters.\n")
+            continue
 
-    with open(f"{name}", "w", encoding="utf-8") as fp:
+        height: int = int(input("How high? (chars) "))
+        if height <= 1:
+            print("Please enter a value of at least two characters.\n")
+            continue
+
+        break
+
+    # Add file extension if not present
+    if name[-4:] != ".gol":
+        name += ".gol"
+
+    with open(os.path.join("..", "boards", name), "w", encoding="utf-8") as fp:
         for i in range(height):
             # Format and write chars entered by user
-            fp.write("".join(controlled_input(f"Enter line No. {i + 1}: ", width)))
+            fp.write(
+                "".join(controlled_input(f"Enter line No. {i + 1}: ", width))
+            )
 
             if i != height - 1:
                 # Don't write last newline
                 fp.write("\n")
+    print("File created successfully!")
 
 
 def count_neighbors(board: list[list[bool]], row: int, col: int) -> int:
@@ -168,8 +239,8 @@ def count_neighbors(board: list[list[bool]], row: int, col: int) -> int:
     """
     # Relative position of the cells neighbours
     neighbour_rel_pos: list = [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
-                 (row, col - 1),                     (row, col + 1),
-                 (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
+                               (row, col - 1),                     (row, col + 1),
+                               (row + 1, col - 1), (row + 1, col), (row + 1, col + 1),]
 
     live_neighbors: int = 0
     for i, j in neighbour_rel_pos:
@@ -197,7 +268,7 @@ def update_board(board: list[list[bool]]) -> list[list[bool]]:
     return new_board
 
 
-def print_board(local_board: list[list[bool]], character: str=' ') -> None:
+def print_board(local_board: list[list[bool]], character: str = " ") -> None:
     """Print the current state of the board.
 
     Colorama is used to draw colored characters.
@@ -206,17 +277,20 @@ def print_board(local_board: list[list[bool]], character: str=' ') -> None:
     """
     live_cells = 0
 
+    os.system("cls")  # Clear the terminal
+
     for row in local_board:
         for cell in row:
             # Color only if cell is alive
             if cell:
-                print(f"{Back.GREEN}{character} {Back.RESET}", end='')
+                print(f"{Back.GREEN}{character} {Back.RESET}", end="")
                 live_cells += 1
             else:
-                print(f"{character} ", end='')
+                print(f"{character} ", end="")
         print()  # New line after row
 
     if live_cells == 0:
+        # Entire board is dead
         end_game()
 
 
@@ -229,30 +303,14 @@ def end_game() -> None:
 
 if __name__ == "__main__":
     display_welcome()
-    # Get filename to import from command line args
-    FILENAME: str = sys.argv[1] if len(sys.argv) > 1 else False
-    # Initialize board to false to check which configuration works
+    global_board = get_start_board()
 
-    if FILENAME:
-        try:
-            global_board = import_from_file(sys.argv[1])
+    # Avoid stuck screens that only include still lifes
+    last_board:list[list[bool]] = []
 
-        except FileNotFoundError:
-            # Start level editor
-            print("Your file was not found. The level editor will be started ",
-                  "so you can make your own starting configuration.")
-            create_level(FILENAME)
-            # TODO restart automatically
-            input("Restart the program to see your changes.")
-            sys.exit(0)
-    else:
-        global_board = generate_random_board()
-
-    last_board = []
-
+    # Main game loop
     while last_board != global_board:
         print_board(global_board)
         last_board = global_board
         global_board = update_board(global_board)
         sleep(0.3)
-        os.system("cls")
